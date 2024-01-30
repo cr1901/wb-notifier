6aadc1a2918a83d67a6a5c135f09d7c940987e8b
William D. Jones <thor0505@comcast.net>
2024-01-30 06:05:52 -0500

    Add features to main crate.

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,7 +1,15 @@
-use std::error::Error;
-use wb_notifier_client::Client;
-use wb_notifier_proto::{Echo, EchoResponse, SetLed};
+use cfg_if::cfg_if;
 
+cfg_if! {
+    if #[cfg(feature="client")] {
+        use wb_notifier_client::Client;
+        use wb_notifier_proto::{Echo, EchoResponse, SetLed};
+    }
+}
+
+use std::error::Error;
+
+#[cfg(feature="client")]
 fn main() -> Result<(), Box<dyn Error>> {
     let mut client = Client::new();
     client.connect("127.0.0.1:12000")?;
@@ -15,6 +23,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let res: Result<String, _> = client.raw::<Echo, EchoResponse, _, _, _>("bad/path", "hello!", &mut buf);
     println!("{:?}", res);
+
+    Ok(())
+}
+
+#[cfg(not(feature="client"))]
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("client feature not enabled");
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
-use smol;
-use wb_notifier_server::Server;
+use cfg_if::cfg_if;
 
-use smol::LocalExecutor;
-use std::{error::Error, rc::Rc};
+cfg_if! {
+    if #[cfg(feature="server")] {
+        use smol;
+        use wb_notifier_server::Server;
 
+        use smol::LocalExecutor;
+        use std::rc::Rc;
+    }
+}
+
+use std::error::Error;
+
+#[cfg(feature="server")]
 fn main() -> Result<(), Box<dyn Error>> {
     let server = Server::new();
     let ex = Rc::new(LocalExecutor::new());
     smol::block_on(ex.run(server.main_loop(ex.clone())))?;
+    Ok(())
+}
+
+#[cfg(not(feature="server"))]
+fn main() -> Result<(), Box<dyn Error>> {
+    println!("server feature not enabled");
+
     Ok(())
 }
